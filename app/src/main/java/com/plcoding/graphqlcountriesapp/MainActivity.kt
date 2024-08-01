61eed0fa3f1cdd4c65a3e337d6ff6a776ad1125d
Ali Sadeghian <alisadeghian@alis-MacBook-Pro-7.local>
2024-07-31 22:52:52 -0400

    Remove redundant imports

--- app/src/main/java/com/plcoding/graphqlcountriesapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/MainActivity.kt
@@ -3,15 +3,8 @@ package com.plcoding.graphqlcountriesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.plcoding.graphqlcountriesapp.domain.CountriesViewModel
 import com.plcoding.graphqlcountriesapp.presentation.CountriesScreen
